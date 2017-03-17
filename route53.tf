296d8e320d20945231abfc66c02d616a57047b9c
Atlas Kerr <atlaskerr@gmail.com>
2017-03-17 09:41:50 -0500

    changes 'dev_stage_prod' to a more generic 'multizone'

--- route53.tf
+++ route53.tf
@@ -1,12 +1,12 @@
 resource "aws_route53_zone" "main" {
-	count	= "${ var.dev_stage_prod == 1 ? 0 : 1 }"
+	count	= "${ var.multizone == 1 ? 0 : 1 }"
 
 	name	= "${ var.base_domain }"
 	vpc_id	= "${ var.vpc_id }"
 }
 
-resource "aws_route53_zone" "dev_stage_prod" {
-	count	= "${ var.dev_stage_prod == 1 ? length(split(",", var.zones)) : 0 }"
+resource "aws_route53_zone" "multizone" {
+	count	= "${ var.multizone == 1 ? length(split(",", var.zones)) : 0 }"
 
 	name	= "${ element(split(",", var.zones), count.index) }.${ var.base_domain }"
 	vpc_id	= "${ var.vpc_id }"
