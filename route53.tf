c254c09961b5463b2faa213db53ab57e476e19df
Atlas Kerr <atlaskerr@gmail.com>
2017-03-21 14:39:03 -0500

    removed unneccessary ternary ==1 format

--- route53.tf
+++ route53.tf
@@ -1,12 +1,12 @@
 resource "aws_route53_zone" "main" {
-	count	= "${ var.multi_zone == 1 ? 0 : 1 }"
+	count	= "${ var.multi_zone ? 0 : 1 }"
 
 	name	= "${ var.base_domain }"
 	vpc_id	= "${ var.vpc_id }"
 }
 
 resource "aws_route53_zone" "multi_zone" {
-	count	= "${ var.multi_zone == 1 ? length(split(",", var.zones)) : 0 }"
+	count	= "${ var.multi_zone ? length(split(",", var.zones)) : 0 }"
 
 	name	= "${ element(split(",", var.zones), count.index) }.${ var.base_domain }"
 	vpc_id	= "${ var.vpc_id }"
